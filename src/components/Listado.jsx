0600f02fc0c2f8613d572b5e2d54a04dc4f20f4a
eliosito <eliovillarroelsandoval@gmail.com>
2026-06-29 22:53:02 -0300

    uso de api

--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -5,8 +5,9 @@ import axios from 'axios'
 
 
 export default function Listado() {
-     const [personas,setPersonas] = useState([])
-  useEffect(() => {
+  const [personas, setPersonas] = useState([])
+
+  const actualizar = () => {
     const url = "https://backend-septimos.ctpoba.edu.ar/api/personas"
 
     const config = {
@@ -21,12 +22,32 @@ export default function Listado() {
       .catch((error) => {
         console.error(error)
       })
+  }
+
+  useEffect(() => {
+    actualizar()
   }, [])
 
   const eliminar = (persona_id) => {
+    const url = `https://backend-septimos.ctpoba.edu.ar/api/personas/${persona_id}`
+    const config = {
+      headers: { Authorization: "48354750" }
+    }
 
-
+    axios.delete(url, config)
+      .then((resp) => {
+        console.log(resp.data);
+        alert("Se elimino la persona")
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => {
+        actualizar()
+      })
   }
+
+
   return (
     <div style={{ flex: 3 }}>
       <h1>Listado</h1>
